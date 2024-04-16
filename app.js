239657ba756e1e05a17e91787028872cc27c5463
Ingrid Ehrich <156666553+ingridehrich@users.noreply.github.com>
2024-04-16 09:10:40 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+alert('Boas vindas ao jogo do número secreto');
+
+let numeroMax = 100;
+
+let numeroSecreto = parseInt(Math.random()* numeroMax + 1 );
+console.log(numeroSecreto); 
+
+let chute;
+let tentativas = 1;
+
+while(chute!= numeroSecreto){
+    
+    chute = prompt(`Escolha um numero entre 1 e ${numeroMax}`);
+    
+    if (chute == numeroSecreto) {
+        break;      
+    }
+    else{
+    
+        if(chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute}` );
+            }
+        else{
+            alert(`O número secreto é maior que ${chute}` )
+        }  
+        tentativas++
+    }
+}
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} 
+com ${tentativas} ${palavraTentativa}!`)
+
+//if(tentativas>1){
+//    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} 
+//    com ${tentativas} tentativas!`);
+//} else{
+//    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} 
+//   com ${tentativas} tentativa!`);}
+
